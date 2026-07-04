a399d1be6535d2740b221b170eeda0589a818ee0
$artnp <99397593+artnp@users.noreply.github.com>
2026-07-05 01:48:39 +0700

    Delete hist_da2e931c-90a4-454c-af11-54df274cc8f3_step41.py

--- hist_da2e931c-90a4-454c-af11-54df274cc8f3_step41.py
+++
@@ -1,6 +0,0 @@
-    if mode_flag == "--clean":
-        process_clean_only(target_path)
-    elif mode_flag in ["--donate", "--donate-no-paste"]:
-        process_donate(target_path)
-    else:
-        process_donate()
