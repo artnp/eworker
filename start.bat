c058a1f020405c2c9caeac63c34a2cafeb6df6b8
$artnp <99397593+artnp@users.noreply.github.com>
2026-07-14 03:15:44 +0700

    Delete start.bat

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-if "%~1"=="" (
-    start "" /B node_modules\electron\dist\electron.exe . 2>nul
-) else (
-    start "" /B node_modules\electron\dist\electron.exe . "%~1" 2>nul
-)
