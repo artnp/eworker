9cdeddcb3c5a1e37020661838324deee159f5dec
$artnp <99397593+artnp@users.noreply.github.com>
2026-07-13 15:42:01 +0700

    Create ??????????.bat

---
+++ มาร์คจุดที.bat
@@ -0,0 +1,9 @@
+﻿@echo off
+title MarkPoints - Uploading...
+if "%~1"=="" (
+    echo [!] Drag and drop image/PDF onto this icon.
+    pause
+    exit
+)
+powershell -ExecutionPolicy Bypass -File "%~dp0MarkPoints.ps1" "%~1"
+exit
