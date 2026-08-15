e4d0448da77ca8bc230f8b818eb3701f3859d1fa
$artnp <99397593+artnp@users.noreply.github.com>
2026-08-15 23:26:27 +0700

    Update Facebook_Bot/minimize_chrome.ps1

--- Facebook_Bot/minimize_chrome.ps1
+++ Facebook_Bot/minimize_chrome.ps1
@@ -9,9 +9,14 @@ Add-Type @"
   }
 "@
 
-# Move Chrome window offscreen (X=10000, Y=10000) so it continues rendering in background without throttling
-Get-Process chrome -ErrorAction SilentlyContinue | Where-Object {$_.MainWindowHandle -ne 0} | ForEach-Object {
+# Move & tuck window to bottom-right corner of Screen 2 (X=3800, Y=1000)
+Get-Process -ErrorAction SilentlyContinue | Where-Object { 
+  ($_.ProcessName -like "*chrome*" -or $_.ProcessName -like "*chromium*" -or $_.ProcessName -like "*msedge*") -and 
+  $_.MainWindowHandle -ne [IntPtr]::Zero 
+} | ForEach-Object {
   [Win32]::ShowWindow($_.MainWindowHandle, 4) | Out-Null # SW_SHOWNOACTIVATE
-  [Win32]::SetWindowPos($_.MainWindowHandle, [IntPtr]::Zero, 10000, 10000, 1280, 800, 0x0054) | Out-Null # SWP_NOACTIVATE | SWP_NOZORDER | SWP_SHOWWINDOW
-  Write-Host "Moved Chrome window offscreen: $($_.Id)"
+  [Win32]::SetWindowPos($_.MainWindowHandle, [IntPtr]1, 3800, 1000, 1280, 800, 0x0054) | Out-Null # SWP_NOACTIVATE | SWP_NOZORDER | SWP_SHOWWINDOW
+  Write-Host "Tucked away $($_.ProcessName) ($($_.Id)) on Screen 2"
 }
+
+
